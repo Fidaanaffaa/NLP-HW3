5cfa7fe6fc567acb57bd2b4e677ecbe627936654
Ron <nrdronys@gmail.com>
2022-12-19 17:11:17 +0200

    works

--- exercise_blanks.py
+++ exercise_blanks.py
@@ -9,6 +9,8 @@ import operator
 import data_loader
 import pickle
 import tqdm
+from statistics import mean
+
 
 # ------------------------------------------- Constants ----------------------------------------
 
@@ -115,8 +117,10 @@ def get_w2v_average(sent, word_to_vec, embedding_dim):
     :param embedding_dim: the dimension of the word embedding vectors
     :return The average embedding vector as numpy ndarray.
     """
-    
-    return
+
+    embedding_sent_mean = np.mean(sentence_to_embedding(sent, word_to_vec, 1, embedding_dim), axis=0)
+
+    return embedding_sent_mean
 
 
 def get_one_hot(size, ind):
@@ -126,7 +130,10 @@ def get_one_hot(size, ind):
     :param ind: the entry index to turn to 1
     :return: numpy ndarray which represents the one-hot vector
     """
-    return
+
+    one_hot_vector = np.zeros(size)
+    one_hot_vector[ind] = 1
+    return one_hot_vector
 
 
 def average_one_hots(sent, word_to_ind):
@@ -137,7 +144,28 @@ def average_one_hots(sent, word_to_ind):
     :param word_to_ind: a mapping between words to indices
     :return:
     """
-    return
+
+    vocabulary = word_to_ind.keys()
+    text_list = sent.text
+    # test if all words in the sentence are unknown words
+    i = 0
+    for word in text_list:
+        if i == len(text_list):
+            return np.zeros(len(text_list))
+        if word in vocabulary:
+            break
+        else:
+            i += 1
+    # initialize param
+    i = 0
+    one_hot_vectors_matrix = np.zeros(shape=((len(text_list), len(word_to_ind))))
+    for word in text_list:
+        one_hot_vectors_matrix[i] = get_one_hot(len(word_to_ind), word_to_ind[word])
+        i += 1
+    sum_vectors = one_hot_vectors_matrix.sum(axis=0)  # axis = 0 sum all the cols
+    embedding_vector = sum_vectors / len(word_to_ind)  # normalize the sum vector
+
+    return embedding_vector
 
 
 def get_word_to_ind(words_list):
@@ -147,7 +175,15 @@ def get_word_to_ind(words_list):
     :param words_list: a list of words
     :return: the dictionary mapping words to the index
     """
-    return
+    bag_of_words_dict, vocabulary = {}, set()
+
+    bag_of_words_curr_index = 0
+    for word in words_list:
+        if word not in bag_of_words_dict.keys():
+            vocabulary.add(word)
+            bag_of_words_dict[word] = bag_of_words_curr_index
+            bag_of_words_curr_index += 1
+    return bag_of_words_dict
 
 
 def sentence_to_embedding(sent, word_to_vec, seq_len, embedding_dim=300):
@@ -160,7 +196,13 @@ def sentence_to_embedding(sent, word_to_vec, seq_len, embedding_dim=300):
     :param embedding_dim: the dimension of the w2v embedding
     :return: numpy ndarray of shape (seq_len, embedding_dim) with the representation of the sentence
     """
-    return
+    vectors_embedding_matrix = np.zeros(shape=(seq_len, embedding_dim))
+    n = len(sent.text) if len(sent.text) < seq_len else seq_len
+    for i in range(n):
+        cur_word = sent.text[i]
+        if cur_word in word_to_vec.keys():
+            vectors_embedding_matrix[i] = word_to_vec[cur_word]
+    return vectors_embedding_matrix
 
 
 class OnlineDataset(Dataset):
@@ -288,13 +330,17 @@ class LogLinear(nn.Module):
     general class for the log-linear models for sentiment analysis.
     """
     def __init__(self, embedding_dim):
-        return
+        super().__init__()
+        self._log_linear_layer = nn.Linear(in_features=embedding_dim, out_features=1)
 
     def forward(self, x):
-        return
+        # Not clear
+        return self._log_linear_layer(x)
 
     def predict(self, x):
-        return
+        layer_o = self._log_linear_layer(x)
+        out = nn.Sigmoid()(layer_o)
+        return out
 
 
 # ------------------------- training functions -------------
@@ -308,8 +354,17 @@ def binary_accuracy(preds, y):
     :param y: a vector of true labels
     :return: scalar value - (<number of accurate predictions> / <number of examples>)
     """
+    binary_predictions = [0 if probability < 0.5 else 1 for probability in preds]
 
-    return
+    true_positive = 0
+    true_negative = 0
+    for i in range(len(preds)):
+        if binary_predictions[i] == y[i]:
+            if y[i] == 1:
+                true_positive += 1
+            elif y[i] == 0:
+                true_negative += 1
+    return (true_positive + true_negative) / len(preds)
 
 
 def train_epoch(model, data_iterator, optimizer, criterion):
@@ -321,9 +376,36 @@ def train_epoch(model, data_iterator, optimizer, criterion):
     :param optimizer: the optimizer object for the training process.
     :param criterion: the criterion object for the training process.
     """
+    model.train()
+    activation_function = nn.Sigmoid()
+    accuracy_list, loss_weights_list = [], []
 
-    return
+    for item in data_iterator:
+        word_sample, tag_sample = item[0].float(), item[1].float()
 
+        optimizer.zero_grad()
+        y_predict = model.forward(word_sample)
+
+        y_real = tag_sample.view(tag_sample.shape[0], 1)
+
+        loss_tensor = criterion(y_predict, y_real)
+
+        loss_weights_list.append(loss_tensor.item())
+
+        loss_tensor.backword()
+
+        optimizer.step()
+
+        sigmoied_y_predict = activation_function(y_predict)
+
+        accuracy = binary_accuracy(sigmoied_y_predict, y_real)
+
+        accuracy_list.append(accuracy)
+
+    mle_results = mean(loss_weights_list)
+    average_accuracy = mean(accuracy_list)
+
+    return mle_results, average_accuracy
 
 def evaluate(model, data_iterator, criterion):
     """
@@ -333,7 +415,32 @@ def evaluate(model, data_iterator, criterion):
     :param criterion: the loss criterion used for evaluation
     :return: tuple of (average loss over all examples, average accuracy over all examples)
     """
-    return
+    model.eval()
+    activation_function = nn.Sigmoid()
+    accuracy_list, loss_weights_list = [], []
+
+    for item in data_iterator:
+        word_sample, tag_sample = item[0].float(), item[1].float()
+
+        y_predict = model.forward(word_sample)
+
+        y_real = tag_sample.view(tag_sample.shape[0], 1)
+
+        loss_tensor = criterion(y_predict, y_real)
+
+        loss_weights_list.append(loss_tensor.item())
+
+
+        sigmoied_y_predict = activation_function(y_predict)
+
+        accuracy = binary_accuracy(sigmoied_y_predict, y_real)
+
+        accuracy_list.append(accuracy)
+
+    mle_results = mean(loss_weights_list)
+    average_accuracy = mean(accuracy_list)
+
+    return mle_results, average_accuracy
 
 
 def get_predictions_for_data(model, data_iter):
@@ -359,13 +466,42 @@ def train_model(model, data_manager, n_epochs, lr, weight_decay=0.):
     :param lr: learning rate to be used for optimization
     :param weight_decay: parameter for l2 regularization
     """
-    return
+
+    train_loss_list, train_accuracy_list = np.zeros(n_epochs), np.zeros(n_epochs)
+    val_train_loss_list, val_accuracy_list = np.zeros(n_epochs), np.zeros(n_epochs)
+
+    solver = torch.optim.Adam(params=model.parameters(), lr=lr, weight_decay=weight_decay)
+
+    train_data_iterator = data_manager.get_torch_iterator(data_subset=TRAIN)
+    val_data_iterator = data_manager.get_torch_iterator(data_subset=VAL)
+
+    for i in range(n_epochs):
+        train_loss_list[i], train_accuracy_list[i] = train_epoch(model, train_data_iterator, solver, nn.BCEWithLogitsLoss)
+        val_train_loss_list[i], val_accuracy_list[i] = evaluate(model, train_data_iterator, solver)
+
+
+    return train_loss_list, train_accuracy_list, val_train_loss_list, val_accuracy_list, solver
 
 
 def train_log_linear_with_one_hot():
-    """
-    Here comes your code for training and evaluation of the log linear model with one hot representation.
-    """
+    w_decay = 0.001
+    learning_rate, n_epoch, batches_size = 0.01, 20, 64
+
+    train_data_manger = DataManager(data_type=ONEHOT_AVERAGE, batch_size=batches_size)
+    test_set = train_data_manger.get_torch_iterator(data_subset=TEST)
+    embedding_dim = train_data_manger.get_input_shape()[0]  # need to unpack it cuz its a tuple
+    log_linear_model = LogLinear(embedding_dim=embedding_dim)
+
+    # training phase
+    train_loss, train_accuracy, train_validation_loss, train_validation_accuracy, trained_solver = train_model(log_linear_model,
+                                                                                                       train_data_manger,
+                                                                                                       n_epoch,
+                                                                                                       learning_rate,
+                                                                                                       w_decay)
+
+    print(f'train loss list is = {train_loss} \n train accuracy list is = { train_accuracy}')
+    print(f'validation loss list is = {train_validation_loss} \n validation accuracy list is = { train_validation_accuracy}')
+
     return
 
 
